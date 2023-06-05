f71173163106fa40813ddbd9d6444a1e77c3d6d3
Matheus <matheusfsantos.152@gmail.com>
2023-06-05 11:42:11 -0300

    add

--- Assets/Cods/Bot2.cs
+++ Assets/Cods/Bot2.cs
@@ -42,71 +42,84 @@ public class Bot2 : MonoBehaviour
                 }
                 else if (life <= 5900 & life > 5200)
                 {
+                    ataques[0].gameObject.SetActive(false);
                     ataques[1].gameObject.SetActive(true);
                     ataques[2].gameObject.SetActive(true);
                 }
                 else if (life <= 5200 & life > 4600)
                 {
-                    ataques[0].gameObject.SetActive(false);
                     ataques[1].gameObject.SetActive(false);
-                    ataques[2].gameObject.SetActive(true);
+                    ataques[2].gameObject.SetActive(false);
                     ataques[4].gameObject.SetActive(true);
-                    bombas[4].gameObject.SetActive(true);
-                    bombas[5].gameObject.SetActive(true);
+                    
                 }
                 else if (life <= 4600 & life > 3700)
                 {
                     ataques[4].gameObject.SetActive(false);
-                    bombas[4].gameObject.SetActive(false);
-                    bombas[5].gameObject.SetActive(false);
                     ataques[3].gameObject.SetActive(true);
-                    bombas[0].gameObject.SetActive(true);
-                    bombas[1].gameObject.SetActive(true);
                 }
                 else if (life <= 3700 & life > 3000)
                 {
-                    ataques[2].gameObject.SetActive(false);
                     ataques[3].gameObject.SetActive(false);
-                    bombas[0].gameObject.SetActive(false);
-                    bombas[1].gameObject.SetActive(false);
-                    ataques[4].gameObject.SetActive(true);
-                    ataques[5].gameObject.SetActive(true);
-                    bombas[2].gameObject.SetActive(true);
-                    bombas[3].gameObject.SetActive(true);
+                    ataques[1].gameObject.SetActive(true);
+                    bombas[0].gameObject.SetActive(true);
+                    bombas[1].gameObject.SetActive(true);
                 }
             }
             else
             {
                 if (life > 2350)
                 {
-                    ataques[4].gameObject.SetActive(false);
-                    ataques[5].gameObject.SetActive(false);
+                    ataques[1].gameObject.SetActive(false);
+                    bombas[0].gameObject.SetActive(false);
+                    bombas[1].gameObject.SetActive(false);
+                    bombas[2].gameObject.SetActive(true);
+                    bombas[3].gameObject.SetActive(true);
+                    bombas[4].gameObject.SetActive(true);
+                    bombas[5].gameObject.SetActive(true);
+                }
+                else if (life <= 2350 & life > 2325)
+                {
                     bombas[2].gameObject.SetActive(false);
                     bombas[3].gameObject.SetActive(false);
-                    ataques[6].gameObject.SetActive(true);
-                    ataques[7].gameObject.SetActive(true);
+                    bombas[4].gameObject.SetActive(false);
+                    bombas[5].gameObject.SetActive(false);
+                    laisers[0].gameObject.SetActive(true);
                 }
-                else if (life <= 2350 & life > 1200)
+                else if (life <= 2325 & life > 2300)
                 {
-                    ataques[6].gameObject.SetActive(false);
-                    ataques[7].gameObject.SetActive(false);
-                    ataques[8].gameObject.SetActive(true);
-                    ataques[9].gameObject.SetActive(true);
+                    laisers[0].gameObject.SetActive(false);
+                    laisers[1].gameObject.SetActive(true);
+                }
+                else if (life <= 2300 & life > 2280)
+                {
+                    laisers[1].gameObject.SetActive(false);
+                    laisers[2].gameObject.SetActive(true);
+                }
+                else if (life <= 2280 & life > 2260)
+                {
+                    laisers[2].gameObject.SetActive(false);
+                    laisers[3].gameObject.SetActive(true);
+                }
+                else if (life <= 2260 & life > 2000)
+                {
+                    laisers[3].gameObject.SetActive(false);
                     ataques[0].gameObject.SetActive(true);
+                    laisers[4].gameObject.SetActive(true);
+                    laisers[5].gameObject.SetActive(true);
+                }
+                else if (life <= 2000 & life > 1200)
+                {
+                    laisers[6].gameObject.SetActive(true);
+                    laisers[7].gameObject.SetActive(true);
                 }
                 else if (life <= 1200 & life > 500)
                 {
-                    ataques[8].gameObject.SetActive(false);
-                    ataques[9].gameObject.SetActive(false);
                     ataques[0].gameObject.SetActive(false);
-                    bombas[0].gameObject.SetActive(true);
-                    bombas[1].gameObject.SetActive(true);
-                    bombas[6].gameObject.SetActive(true);
-                    bombas[7].gameObject.SetActive(true);
-                    bombas[8].gameObject.SetActive(true);
-                    bombas[9].gameObject.SetActive(true);
-                    bombas[10].gameObject.SetActive(true);
-                    bombas[11].gameObject.SetActive(true);
+                    laisers[4].gameObject.SetActive(false);
+                    laisers[5].gameObject.SetActive(false);
+                    laisers[6].gameObject.SetActive(false);
+                    laisers[7].gameObject.SetActive(false);
                 }
                 else if (life <= 500 & life > 0)
                 {
